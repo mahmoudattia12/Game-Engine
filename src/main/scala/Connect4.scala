80ccdbcfd36e1e5934e36d1ed92e38527903439b
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-07-03 23:30:44 +0300

    solving a bug

--- src/main/scala/Connect4.scala
+++ src/main/scala/Connect4.scala
@@ -12,21 +12,21 @@ def connect4Drawer(currState: (Array[Array[String]], Boolean), winner: String): 
       setVerticalAlignment(SwingConstants.CENTER)
       setForeground(if (currState._1(row)(col) == "1") Color.red else if (currState._1(row)(col) == "2") Color.yellow else Color.white)
       setText("\uD83D\uDD34")
-      setBorder(BorderFactory.createLineBorder(Color.blue))
-      setBackground(Color.blue)
+      setBorder(BorderFactory.createLineBorder(Color.decode("#560b74")))
+      setBackground(Color.decode("#560b74"))
       setFocusable(false)
       setOpaque(true)
       addMouseListener(new MouseAdapter() {
         override def mouseEntered(e: MouseEvent): Unit = {
-          setBackground(new Color(64, 167, 234)) // Change the background color on hover
+          setBackground(new Color(213, 133, 234)) // Change the background color on hover
         }
 
         override def mouseExited(e: MouseEvent): Unit = {
           setForeground(if (currState._1(row)(col) == "1") Color.red else if (currState._1(row)(col) == "2") Color.yellow else Color.white)
-          setBackground(Color.blue)
+          setBackground(Color.decode("#560b74"))
         }
       })
-      if (winner == "none") {
+      if (winner == winnerStatus.noWin) {
         addActionListener(new ActionListener() {
           def actionPerformed(e: ActionEvent): Unit = {
             if (currState._1(row)(col) == null) {
@@ -34,7 +34,8 @@ def connect4Drawer(currState: (Array[Array[String]], Boolean), winner: String): 
               // Play audio clip
               val path = "D:/CSED/level2/2nd semester/programming paradigms/project/Phase-1/Game-Engine/audio/".concat(if (currState._2) "goal1.wav" else "goal2.wav")
               generateAudio(path)
-              connect4Controller(currState, input)
+              val res = connect4Controller(currState, input)
+              connect4Drawer(res._1, res._2)
             }
           }
         })
@@ -51,18 +52,18 @@ def connect4Drawer(currState: (Array[Array[String]], Boolean), winner: String): 
 
         }
       }
-      setBackground(Color.blue)
+      setBackground(Color.decode("#560b74"))
     }
   }
   val colors = (Color.red, Color.yellow, Color.white)
     if (getMainFrame("Connect 4") == null) {
-      createMainFrame(createLabel("Welcome to Connect 4 !"), createGamePanel(),createGamePanel(), "Connect 4", true, createTurnLabel(currState._2, winner, colors))
+      createMainFrame(createLabel("Welcome to Connect 4 !"), createButtonsPanelDA("Connect 4"),createGamePanel(), "Connect 4", createTurnLabel(currState._2, winner, colors))
     }
     else {
-      updateFrame(createLabel("Welcome to Connect 4 !"), createGamePanel(), createGamePanel(), "Connect 4", true, createTurnLabel(currState._2, winner, colors))
+      updateFrame(createLabel("Welcome to Connect 4 !"), createButtonsPanelDA("Connect 4"), createGamePanel(), "Connect 4", createTurnLabel(currState._2, winner, colors))
     }
 }
-def connect4Controller(currState: (Array[Array[String]], Boolean), input: String) = {
+def connect4Controller(currState: (Array[Array[String]], Boolean), input: String):((Array[Array[String]], Boolean), String) = {
   def getRow(col: Int): (Int) = {
     for (i <- (0 to 5)) {
       currState._1(i)(col) match {
@@ -82,66 +83,66 @@ def connect4Controller(currState: (Array[Array[String]], Boolean), input: String
     val currPlay = currState._1(row)(col)
 
     def checkVertical(r : Int, count : Int):Boolean ={
-      if(count == 4) true
+      if(count >= 4) true
       else if(r+1 > 5 || currState._1(r+1)(col) != currPlay) false
       else checkVertical(r+1, count+1)
     }
 
     def checkHorizontalLeft(c: Int, count:Int):Int ={
-      if(count == 4) count
+      if(count >= 4) count
       else if(c-1 < 0 || currState._1(row)(c-1) != currPlay) count
       else checkHorizontalLeft(c-1, count+1)
     }
 
     def checkHorizontalRight(c:Int, count:Int):Int={
-      if (count == 4) count
+      if (count >= 4) count
       else if (c + 1 > 6 || currState._1(row)(c+1) != currPlay) count
       else checkHorizontalRight(c + 1, count + 1)
     }
     def checkHorizontal():Boolean={
-      if(checkHorizontalLeft(col,1) + checkHorizontalRight(col,1) - 1 == 4) true else false
+      if(checkHorizontalLeft(col,1) + checkHorizontalRight(col,1) - 1 >= 4) true else false
     }
 
     def diagonalUR(r:Int, c:Int, count:Int):Int ={
-      if(count == 4) count
+      if(count >= 4) count
       else if( r-1 < 0 || c+1 > 6 || currState._1(r-1)(c+1) != currPlay) count
       else diagonalUR(r-1,c+1, count+1)
     }
 
     def diagonalDL(r: Int, c: Int, count: Int): Int = {
-      if (count == 4) count
+      if (count >= 4) count
       else if (r + 1 > 5 || c - 1 < 0 || currState._1(r + 1)(c - 1) != currPlay) count
       else diagonalDL(r + 1, c - 1, count + 1)
     }
 
     def diagonalUL(r: Int, c: Int, count: Int): Int = {
-      if (count == 4) count
+      if (count >= 4) count
       else if (r - 1 < 0 || c - 1 < 0 || currState._1(r - 1)(c - 1) != currPlay) count
       else diagonalUL(r - 1, c - 1, count + 1)
     }
 
     def diagonalDR(r: Int, c: Int, count: Int): Int = {
-      if (count == 4) count
+      if (count >= 4) count
       else if (r + 1 > 5 || c + 1 > 6 || currState._1(r + 1)(c + 1) != currPlay) count
       else diagonalDR(r + 1, c + 1, count + 1)
     }
 
     def checkDiagonal():Boolean ={
-      if(diagonalUR(row,col,1) + diagonalDL(row,col,1)-1 == 4) true
-      else if(diagonalUL(row,col,1) + diagonalDR(row,col,1) - 1 == 4) true
+      if(diagonalUR(row,col,1) + diagonalDL(row,col,1)-1 >= 4) true
+      else if(diagonalUL(row,col,1) + diagonalDR(row,col,1) - 1 >= 4) true
       else false
     }
-    if(checkVertical(row, 1) || checkHorizontal() || checkDiagonal()) currPlay else "none"
+    if(checkVertical(row, 1) || checkHorizontal() || checkDiagonal()) currPlay else winnerStatus.noWin
   }
   //function to validate and set the cell in the currState
   def setCell(index: (Int, Int)): (Boolean,String) = {
     index._1 match {
-      case -1 => (false, "none")
+      case -1 => (false, winnerStatus.noWin)
       case _ =>
         if (currState._2) currState._1(index._1)(index._2) = "1" else currState._1(index._1)(index._2) = "2"
         val winner = getWinner(index)
-        if (winner == "none") {
-          if (isFull(6, 7, currState._1)) (true, "full") else (true, winner)
+        if (winner == winnerStatus.noWin) {
+          if (isFull(6, 7, currState._1)) (true, winnerStatus.full) else (true, winner)
         } else {
           (true, winner)
         }
@@ -150,6 +151,6 @@ def connect4Controller(currState: (Array[Array[String]], Boolean), input: String
 
   val col = input.toInt
   val res = setCell(getRow(col), col)
-  if (res._1) connect4Drawer((currState._1, !currState._2), res._2) else connect4Drawer(currState, res._2)
+  if (res._1) ((currState._1, !currState._2), res._2) else (currState, res._2)
 
 }
