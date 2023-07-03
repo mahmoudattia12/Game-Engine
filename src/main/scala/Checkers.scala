826a59e670b73b176dbf18a58854e838826a9edc
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-07-03 23:29:45 +0300

    enhance checkers gui

--- src/main/scala/Checkers.scala
+++ src/main/scala/Checkers.scala
@@ -1,54 +1,87 @@
 import java.awt.*
+import java.awt.event.*
 import javax.swing.*
 import scala.annotation.tailrec
 
-def checkersDrawers(currState: Array[Array[String]]): Unit = {
-  def createCellLabel(colorB: Color, colorF: Color, text: String): JLabel = {
-    new JLabel() {
-      setFont(new Font("Noto Color Emoji", Font.PLAIN, 40))
+var firstInput: (Int, Int) = (-1, -1)
+def checkersDrawers(currState: (Array[Array[String]], Boolean), winner: String): Unit = {
+  def createCellButton(colorB: Color, colorF: Color, row: Int, col: Int): JButton = {
+    new JButton() {
+      setFont(new Font("Noto Color Emoji", Font.PLAIN, 55))
       setHorizontalAlignment(SwingConstants.CENTER)
       setVerticalAlignment(SwingConstants.CENTER)
       setBackground(colorB)
-      if(text != null) setText("\uD83D\uDD34") else setText(null)
+      if (currState._1(row)(col) != null) setText("\uD83D\uDD34") else setText(null)
       setForeground(colorF)
       setOpaque(true)
+      setFocusable(false)
+      setMargin(new Insets(0, 0, 0, 0))
+
+      addMouseListener(new MouseAdapter() {
+        override def mouseEntered(e: MouseEvent): Unit = {
+          if (colorB == Color.white) setBackground(new Color(200, 200, 200)) else setBackground(new Color(70, 70, 70)) // Change the background color on hover
+        }
+
+        override def mouseExited(e: MouseEvent): Unit = {
+          setForeground(colorF)
+          setBackground(colorB)
+        }
+      })
+      val path = "D:/CSED/level2/2nd semester/programming paradigms/project/Phase-1/Game-Engine/audio/click.wav"
+      if (winner == winnerStatus.noWin) {
+        addActionListener(new ActionListener() {
+          def actionPerformed(e: ActionEvent): Unit = {
+            if (firstInput == (-1, -1)) {
+              if ((currState._2 && currState._1(row)(col) == "1") || (!currState._2 && currState._1(row)(col) == "2")) {
+                generateAudio(path)
+                firstInput = (row, col)
+              }
+            }else if(firstInput != (row, col)) {
+              generateAudio(path)
+              val input = s"${firstInput._1} ${firstInput._2} $row $col"
+              firstInput = (-1, -1)
+              val res = checkersController(currState, input)
+              checkersDrawers(res._1, res._2)
+            }
+          }
+        })
+      }
     }
   }
+
   def createGamePanel(): JPanel = {
-    val gamePanel = new JPanel(new GridLayout(8, 8)) {
+    new JPanel(new GridLayout(8, 8)) {
       (0 until 8).flatMap { row =>
         (0 until 8).map { col =>
-          val foreColor: Color = if (currState(row)(col) == "1") Color.red else Color.blue
+          val foreColor: Color = if (currState._1(row)(col) == "1") Color.red else Color.blue
           if (isSame(row, col)) {
-            add(createCellLabel(Color.white, foreColor, currState(row)(col)))
+            add(createCellButton(Color.white, foreColor, row, col))
           } else {
-            add(createCellLabel(Color.black, foreColor, currState(row)(col)))
+            add(createCellButton(Color.black, foreColor, row, col))
           }
         }
       }
       setBorder(BorderFactory.createLineBorder(Color.BLACK, 3))
-      setBounds(550, 100, 400, 400)
+      setBounds(500, 230, 500, 500)
     }
-    val collectingPanel = new JPanel(new BorderLayout()) {
-      add(createRowPanelReference(8, 40), BorderLayout.WEST)
-      add(gamePanel, BorderLayout.CENTER)
-      add(createColPanelReference(9, 400, 50, Array(" ", "a ", "b", "c", "d", "e", "f", "g", "h")), BorderLayout.SOUTH)
-      setBounds(530, 100, 400, 400)
-    }
-    collectingPanel
   }
+
+  val colors = (Color.red, Color.blue, Color.white)
   if (getMainFrame("Checkers") == null) {
-    createMainFrame(createLabel("Welcome to  Checkers!"), createGamePanel(), "Checkers")
+    createMainFrame(createLabel("Welcome to  Checkers!"), createButtonsPanelDA("Checkers"), createGamePanel(), "Checkers", createTurnLabel(currState._2, winner, colors))
   }
   else {
-    updateFrame(createLabel("Welcome to  Checkers!"), createGamePanel(), "Checkers")
+    updateFrame(createLabel("Welcome to  Checkers!"), createButtonsPanelDA("Checkers"), createGamePanel(), "Checkers", createTurnLabel(currState._2, winner, colors))
   }
 }
-def checkersController(currState: (Array[Array[String]], Boolean), input: String): (Boolean, Array[Array[String]]) = {
+def checkersController(currState: (Array[Array[String]], Boolean), input: String) : ((Array[Array[String]], Boolean), String) = {
   val inputArr = splitString(input)
+  val from = (inputArr(0).toInt, inputArr(1).toInt)
+  val to = (inputArr(2).toInt, inputArr(3).toInt)
+
   def validate(player: Int, oldi: Int, oldj: Int, newi: Int, newj: Int): String = {
     val validSource =
-      if(player == 1)
+      if (player == 1)
         currState._1(oldi)(oldj) == "1"
       else
         currState._1(oldi)(oldj) == "2"
@@ -72,21 +105,23 @@ def checkersController(currState: (Array[Array[String]], Boolean), input: String
       "not valid"
     }
   }
+
   def isWithin(pos: (Int, Int)): Boolean = {
     val (row, col) = pos
     row >= 0 && row < currState._1.length && col >= 0 && col < currState._1(0).length
   }
-  def checkForJmp(i: Int, j: Int, currentTurn: Int): String = {
+
+  def checkForJmp(i: Int, j: Int, currentTurn: Int, isFirst: Boolean): String = {
     if (currentTurn == 1) {
       val rightChild = (i + 1, j + 1)
       val leftChild = (i + 1, j - 1)
-      if (isWithin(rightChild) && currState._1(rightChild._1)(rightChild._2) == "2") {
+      if ((to == rightChild || !isFirst ) && isWithin(rightChild) && currState._1(rightChild._1)(rightChild._2) == "2") {
         val rightOfRightChild = (i + 2, j + 2)
         if (isWithin(rightOfRightChild) && currState._1(rightOfRightChild._1)(rightOfRightChild._2) == null) {
           return "right jmp"
         }
       }
-      if (isWithin(leftChild) && currState._1(leftChild._1)(leftChild._2) == "2") {
+      if ((to == leftChild || !isFirst) && isWithin(leftChild) && currState._1(leftChild._1)(leftChild._2) == "2") {
         val leftOfLeftChild = (i + 2, j - 2)
         if (isWithin(leftOfLeftChild) && currState._1(leftOfLeftChild._1)(leftOfLeftChild._2) == null) {
           return "left jmp"
@@ -96,13 +131,13 @@ def checkersController(currState: (Array[Array[String]], Boolean), input: String
     } else {
       val rightChild = (i - 1, j + 1)
       val leftChild = (i - 1, j - 1)
-      if (isWithin(rightChild) && currState._1(rightChild._1)(rightChild._2) == "1") {
+      if ((to == rightChild || !isFirst) && isWithin(rightChild) && currState._1(rightChild._1)(rightChild._2) == "1") {
         val rightOfRightChild = (i - 2, j + 2)
         if (isWithin(rightOfRightChild) && currState._1(rightOfRightChild._1)(rightOfRightChild._2) == null) {
           return "right jmp"
         }
       }
-      if (isWithin(leftChild) && currState._1(leftChild._1)(leftChild._2) == "1") {
+      if ((to == leftChild || !isFirst) && isWithin(leftChild) && currState._1(leftChild._1)(leftChild._2) == "1") {
         val leftOfLeftChild = (i - 2, j - 2)
         if (isWithin(leftOfLeftChild) && currState._1(leftOfLeftChild._1)(leftOfLeftChild._2) == null) {
           return "left jmp"
@@ -111,68 +146,71 @@ def checkersController(currState: (Array[Array[String]], Boolean), input: String
       null // return null as a String
     }
   }
-  @tailrec
-  def jmpRecursively(i: Int, j: Int, turn: Int): Unit = {
-    if (!isWithin((i, j))) return
-    val check = checkForJmp(i, j, turn)
-    if(turn == 1){
+
+  def jmpRecursively(i: Int, j: Int, turn: Int, isFirst: Boolean): Boolean = {
+    if (!isWithin((i, j))) return false
+    val check = checkForJmp(i, j, turn, isFirst)
+    if (turn == 1) {
       check match {
         case "right jmp" =>
           currState._1(i)(j) = null
           currState._1(i + 1)(j + 1) = null
           currState._1(i + 2)(j + 2) = "1"
-          jmpRecursively(i + 2, j + 2, turn)
+          jmpRecursively(i + 2, j + 2, turn, false)
+          true
         case "left jmp" =>
           currState._1(i)(j) = null
           currState._1(i + 1)(j - 1) = null
           currState._1(i + 2)(j - 2) = "1"
-          jmpRecursively(i + 2, j - 2, turn)
+          jmpRecursively(i + 2, j - 2, turn, false)
+          true
         case _ =>
+          false
       }
-    }else{
+    } else {
       check match {
         case "right jmp" =>
           currState._1(i)(j) = null
           currState._1(i - 1)(j + 1) = null
           currState._1(i - 2)(j + 2) = "2"
-          jmpRecursively(i - 2, j + 2, turn)
+          jmpRecursively(i - 2, j + 2, turn, false)
+          true
         case "left jmp" =>
           currState._1(i)(j) = null
           currState._1(i - 1)(j - 1) = null
           currState._1(i - 2)(j - 2) = "2"
-          jmpRecursively(i - 2, j - 2, turn)
+          jmpRecursively(i - 2, j - 2, turn, false)
+          true
         case _ =>
+          false
 
-    }
+      }
     }
   }
 
-  if(inputArr.length == 2){
-    val from = rephrase_8x8(inputArr(0))
-    val to = rephrase_8x8(inputArr(1))
-    (from, to) match {
-      case ((-1, _),(_,_)) | ((_, -1),(_,_)) | ((_, _),(-1,_)) | ((_, _),(_,-1)) => (false, currState._1)
-      case _ =>
-        //get move status
-        val moveStatus: String = if (currState._2) validate(1, from._1, from._2, to._1, to._2)
-                                else validate(2, from._1, from._2, to._1, to._2)
-        if(moveStatus == "not valid"){
-          (false, currState._1)
-        }else if(moveStatus == "check for jmp"){
-          val check = if (currState._2) checkForJmp(from._1, from._2, 1) else checkForJmp(from._1, from._2, 2)
-          if(check == null) return (false,currState._1)
+  val path = "D:/CSED/level2/2nd semester/programming paradigms/project/Phase-1/Game-Engine/audio/eatChecker.wav"
 
-          if(currState._2) jmpRecursively(from._1, from._2, 1)
-          else  jmpRecursively(from._1, from._2, 2)
+  //get move status
+  val moveStatus: String = if (currState._2) validate(1, from._1, from._2, to._1, to._2)
+  else validate(2, from._1, from._2, to._1, to._2)
+  if (moveStatus == "not valid") {
+    (currState, winnerStatus.noWin)
+  } else if (moveStatus == "check for jmp") {
+    val check = if (currState._2) checkForJmp(from._1, from._2, 1, true) else checkForJmp(from._1, from._2, 2, true)
+    if (check == null) (currState, winnerStatus.noWin)
 
-          (true, currState._1)
-        }else{
-          currState._1(from._1)(from._2) =  null
-          if(currState._2) currState._1(to._1)(to._2) = "1"
-          else currState._1(to._1)(to._2) = "2"
-          (true, currState._1)
-        }
-    }
-  }else (false,currState._1)
+    val res = if (currState._2) jmpRecursively(from._1, from._2, 1, true) else jmpRecursively(from._1, from._2, 2, true)
+    if(res) {
+      generateAudio(path)
+      ((currState._1, !currState._2), winnerStatus.noWin)
+    } else (currState, winnerStatus.noWin)
+  } else {
+    currState._1(from._1)(from._2) = null
+    if (currState._2) currState._1(to._1)(to._2) = "1"
+    else currState._1(to._1)(to._2) = "2"
+    generateAudio(path)
+    ((currState._1, !currState._2), winnerStatus.noWin)
+  }
+
 }
 
